d835975452e70d59b9e60f9116f5d47f9bb45540
v5zhu@qq.com <longzhu20*>
2017-02-23 15:06:06 +0800

    添加自定义异常配置

---
+++ src/main/java/com/heqmentor/core/exception/ErrorConstant.java
@@ -0,0 +1,14 @@
+package com.heqmentor.core.exception;
+
+/**
+ */
+public class ErrorConstant {
+
+    /**
+     * ************************* 错误码常量 ********************************
+     */
+
+    public final static String ERROR_PARAM_IS_NULL = "common.paramIsNull";
+
+
+}
