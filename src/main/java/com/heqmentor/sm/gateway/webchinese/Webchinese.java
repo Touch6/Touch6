8587ed1ab17dccfc8a32dd5edeb7484e9b18e753
v5zhu@qq.com <longzhu20*>
2017-02-24 11:40:44 +0800

    1.增加253短信网关（测试免费） 2.增加中国网建短信网关

---
+++ src/main/java/com/heqmentor/sm/gateway/webchinese/Webchinese.java
@@ -0,0 +1,50 @@
+package com.heqmentor.sm.gateway.webchinese;
+
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+/*
+ * ============================================================================		
+ * = COPYRIGHT		
+ *               PAX TECHNOLOGY, Inc. PROPRIETARY INFORMATION		
+ *   This software is supplied under the terms of a license agreement or		
+ *   nondisclosure agreement with PAX  Technology, Inc. and may not be copied		
+ *   or disclosed except in accordance with the terms in that agreement.		
+ *      Copyright (C) 2017-? PAX Technology, Inc. All rights reserved.		
+ * Description: // Detail description about the function of this module,		
+ *             // interfaces with the other modules, and dependencies. 		
+ * Revision History:		
+ * Date	                 Author	                  Action
+ * 2017/2/24  	         zhuxl@paxsz.com        Create/Add/Modify/Delete
+ * ============================================================================		
+ */
+public class Webchinese {
+    public static void main(String[] args) throws Exception {
+
+        HttpClient client = new HttpClient();
+        PostMethod post = new PostMethod("http://gbk.sms.webchinese.cn");
+        post.addRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=gbk");//在头文件中设置转码
+        NameValuePair[] data = {
+                new NameValuePair("Uid", "Jbaby"),
+                new NameValuePair("Key", "66cb59bc1df5cff7ef5b"),
+                new NameValuePair("smsMob", "13880298929"),
+                new NameValuePair("smsText", "您好，你的验证码是123456【JMBest】")};
+        post.setRequestBody(data);
+
+        client.executeMethod(post);
+        Header[] headers = post.getResponseHeaders();
+        int statusCode = post.getStatusCode();
+        System.out.println("statusCode:" + statusCode);
+        for (Header h : headers) {
+            System.out.println(h.toString());
+        }
+        String result = new String(post.getResponseBodyAsString().getBytes("gbk"));
+        System.out.println(result); //打印返回消息状态
+
+
+        post.releaseConnection();
+
+    }
+}
