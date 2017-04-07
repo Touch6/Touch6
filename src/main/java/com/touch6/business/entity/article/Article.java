ec685ba6f53b6386125db48ad0631d7ab47bcd91
LONG <v5zhu@qq.com>
2017-04-07 08:39:00 +0800

    增加文章实体

---
+++ src/main/java/com/touch6/business/entity/article/Article.java
@@ -0,0 +1,61 @@
+package com.touch6.business.entity.article;
+
+/**
+ * Created by LONG on 2017/4/6.
+ */
+public class Article {
+    private String id;
+    private String author;
+    private String title;
+    private String content;
+    private String category;//
+    private String type;//original：原创  reprint：转载  translation：翻译
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/main/java/com/touch6/business/entity/article/ArticleCategory.java
@@ -0,0 +1,43 @@
+package com.touch6.business.entity.article;
+
+/**
+ * Created by LONG on 2017/4/7.
+ */
+public class ArticleCategory {
+    private String id;
+    private String parentCategory;
+    private String category;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParentCategory() {
+        return parentCategory;
+    }
+
+    public void setParentCategory(String parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+}
